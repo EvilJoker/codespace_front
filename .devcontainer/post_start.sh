1b131a3384bfb954a6b4ba3e36808b1040aa168c
孙启元10312862 <sun.qiyuan@zte.com.cn>
2023-12-15 12:21:28 +0000

    新增任务

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -3,5 +3,5 @@ npm install -g cnpm --registry=https://registry.npm.taobao.org
 
 npm i --force
 cnpm i --force
-npm install curcreate-next-app@14.0.3
-cnpm install  create-next-app@14.0.3 
+# npm install curcreate-next-app@14.0.3
+# cnpm install  create-next-app@14.0.3 
