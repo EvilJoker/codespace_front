a3a46a5f4115ed56c3846f68fc3b3915d92b37f1
孙启元10312862 <sun.qiyuan@zte.com.cn>
2023-12-15 10:51:34 +0800

    增加开发镜像

---
+++ .devcontainer/post_start.sh
@@ -0,0 +1,7 @@
+# npm 安装 app
+npm install -g cnpm --registry=https://registry.npm.taobao.org
+
+npm i --force
+cnpm i --force
+npm install curcreate-next-app@14.0.3
+cnpm install  create-next-app@14.0.3 
